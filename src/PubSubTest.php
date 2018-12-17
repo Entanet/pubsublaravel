afa5c57f497dbcb52c92dc9631eb32e87dd75130
Mark Jones <markjones@enta.net>
2018-12-17 16:51:52 +0000

    Added the unit test class replacement

---
+++ src/PubSubTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: markjones
+ * Date: 2018-12-12
+ * Time: 09:09
+ */
+
+namespace Entanet\PubSubLaravel;
+
+
+namespace Tests;
+
+
+use ReflectionClass;
+
+class EntanetTestCase extends \Tests\TestCase
+{
+    protected $pubSubMock;
+    protected $pubSubInterfaceMock;
+    protected $pubSubKafkaMock;
+    protected $pubSubFacadeMock;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        //Mocking pubSubInterface for subscribing to events.
+        $this->pubSubInterfaceMock = \Mockery::mock('Superbalist\PubSub\PubSubAdapterInterface');
+        $this->app->instance('Superbalist\PubSub\PubSubAdapterInterface', $this->pubSubInterfaceMock);
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    /**
+     *  Allows testing or private/protected functions
+     *
+     * @param $object
+     * @param $methodName
+     * @param array $parameters
+     * @return mixed
+     * @throws \ReflectionException
+     */
+    public function invokeMethod($object, $methodName, array $parameters = []) {
+        $reflection = new ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
+
+}
